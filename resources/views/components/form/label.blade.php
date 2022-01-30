446242ac2b9afa0af02405d5c3e686b09e057ad9
Reniki <hello@reniki.com>
2022-01-30 14:22:22 +0100

    refactoring small fix

--- resources/views/components/form/label.blade.php
+++ resources/views/components/form/label.blade.php
@@ -1,3 +1,3 @@
 @props(['for'])
 
-<label for="$for" {{ $attributes->merge(['class' => "block text-sm font-medium text-gray-800"]) }}>{{ $slot }}</label>
+<label for="{{ $for }}" {{ $attributes->merge(['class' => "block text-sm font-medium text-gray-800"]) }}>{{ $slot }}</label>
