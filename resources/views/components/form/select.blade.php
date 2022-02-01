9fe99fa405884a8fc75d43703b35b68996db62db
Reniki <hello@reniki.com>
2022-02-01 10:31:48 +0100

    wrap in div: fix css (because of space-x)

--- resources/views/components/form/select.blade.php
+++ resources/views/components/form/select.blade.php
@@ -1,17 +1,19 @@
 @props(['name', 'id', 'empty', 'newForm', 'labelName'])
 
-<label for="{{ $id }}" class="block text-sm font-medium text-gray-800">{{ $labelName }}</label>
-<div class="mt-1">
-  <div class="flex justify-between items-center gap-4">
-    <div class="relative w-full">
-      <select name="{{ $name }}" id="{{ $id }}" class="rounded-md shadow-sm block w-full text-gray-500 pr-10 border-2 border-cyan-500 @error($name) border-red-300 text-red-900 placeholder-red-300 @enderror focus:outline-none focus:ring-cyan-500 focus:border-cyan-500 sm:text-sm">
-        <option value="null">{{ $empty }}</option>
-        {{ $slot }}
-      </select>
-      <x-form.error-icon :name="$name" />
+<div>
+  <label for="{{ $id }}" class="block text-sm font-medium text-gray-800">{{ $labelName }}</label>
+  <div class="mt-2">
+    <div class="flex justify-between items-center gap-4">
+      <div class="relative w-full">
+        <select name="{{ $name }}" id="{{ $id }}" class="rounded-md shadow-sm block w-full text-gray-500 pr-10 border-2 border-cyan-500 @error($name) border-red-300 text-red-900 placeholder-red-300 @enderror focus:outline-none focus:ring-cyan-500 focus:border-cyan-500 sm:text-sm">
+          <option value="null">{{ $empty }}</option>
+          {{ $slot }}
+        </select>
+        <x-form.error-icon :name="$name" />
+      </div>
+      @isset($newForm) {{ $newForm }} @endisset
     </div>
-    @isset($newForm) {{ $newForm }} @endisset
   </div>
-</div>
 
-<x-form.error-message :name="$name" />
+  <x-form.error-message :name="$name" />
+</div>
--- resources/views/components/form/textarea.blade.php
+++ resources/views/components/form/textarea.blade.php
@@ -1,15 +1,17 @@
 @props(['name', 'id', 'placeholder'])
 
-<x-form.label for="{{ $id }}">{{ ucfirst($name . ':') }}</x-form.label>
+<div>
+  <x-form.label for="{{ $id }}">{{ ucfirst($name . ':') }}</x-form.label>
 
-<div class="mt-1 relative">
-  <textarea
-    name="{{ $name }}" id="{{ $id }}"
-    cols="30" rows="10"
-    class="block w-full pr-10 border-2 border-cyan-500
-      @error($name) border-red-300 text-red-900 placeholder-red-300 @enderror focus:outline-none focus:ring-cyan-500 focus:border-cyan-500 sm:text-sm rounded-md"
-    {{ $attributes->merge(['placeholder'=>"$placeholder"]) }}>{{ $slot }}</textarea>
-  <x-form.error-icon :name="$name" />
+  <div class="mt-2 relative">
+    <textarea
+      name="{{ $name }}" id="{{ $id }}"
+      cols="30" rows="10"
+      class="block w-full pr-10 border-2 border-cyan-500
+        @error($name) border-red-300 text-red-900 placeholder-red-300 @enderror focus:outline-none focus:ring-cyan-500 focus:border-cyan-500 sm:text-sm rounded-md"
+      {{ $attributes->merge(['placeholder'=>"$placeholder"]) }}>{{ $slot }}</textarea>
+    <x-form.error-icon :name="$name" />
+  </div>
+
+  <x-form.error-message :name="$name" />
 </div>
-
-<x-form.error-message :name="$name" />
