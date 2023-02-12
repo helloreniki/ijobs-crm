1e36fa07ab195dafc5b4b1dfa3f876c25a012ef0
Reniki <hello@reniki.com>
2023-02-12 17:16:25 +0100

    sortByRating fixed

--- resources/views/company/index.blade.php
+++ resources/views/company/index.blade.php
@@ -3,8 +3,8 @@
     {
       @foreach($companies as $company)
       showConfirmModal{{ $company->id }}: false,
-      showEmployees{{ $company->id }}: false,
       @endforeach
+
     }">
     <div class="uppercase font-semibold mb-12">List of Companies</div>
     <div class="flex space-x-3 items-center">
@@ -19,13 +19,22 @@
     <div class="font-bold text-cyan-700 py-1 my-6"><a href="{{ route('company.create') }}">Add new company</a></div>
     <table class="text-sm relative">
       <?php
-            $sortDirectionName = request('sortByName');
+            $sortDirectionName = request('sortByName'); // asc / desc
             if ($sortDirectionName) {
               // if param exists in url swap between asc and desc on second click
               $sortDirectionName === 'asc' ? $sortDirectionName = 'desc' : $sortDirectionName = 'asc';
             } else {
               // if param doesn't exist, make it asc initially
               $sortDirectionName = 'asc';
+            }
+
+            $sortDirectionRating = request('sortByRating'); // asc / desc
+            if ($sortDirectionRating) {
+              // if param exists in url swap between asc and desc on second click
+              $sortDirectionRating === 'asc' ? $sortDirectionRating = 'desc' : $sortDirectionRating = 'asc';
+            } else {
+              // if param doesn't exist, make it asc initially
+              $sortDirectionRating = 'asc';
             }
           ?>
       <thead class="">
@@ -34,20 +43,10 @@
           <th class="p-2">Email</th>
           <th class="p-2">Country</th>
           <th class="p-2">Contacted</th>
-          <?php
-          $sortDirection = request('sortByRating');
-          if ($sortDirection) {
-            // if param exists in url swap between asc and desc on second click
-            $sortDirection === 'asc' ? $sortDirection = 'desc' : $sortDirection = 'asc';
-          } else {
-            // if param doesn't exist, make it asc initially
-            $sortDirection = 'asc';
-          }
-        ?>
           <th class="p-2 flex space-x-3 items-center">
-            <a href="/companies/?sortByRating={{ $sortDirection }}">Rating</a>
+            <a href="/companies/?sortByRating={{ $sortDirectionRating ?? '' }}">Rating</a>
             @if(request('sortByRating'))
-              @if($sortDirection === 'asc')
+              @if($sortDirectionRating === 'asc')
                 {{-- arrow down --}}
                   <svg class="h-3 w-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
@@ -65,18 +64,13 @@
           <th class="p-2"></th>
         </tr>
       </thead>
-      <tbody class="py-3">
+      <tbody class="py-3 divide-y">
         @foreach ($companies as $company)
-        <tr class="text-xs border-b">
-          <td class="py-2 px-2">
+        <tr class="text-xs" >
+          <td class="py-1 px-2">
             <div class="font-semibold"><a href="{{ route('company.show', $company) }}">{{ $company->name }}</a></div>
-            <div class="text-xxs text-gray-500" >{{ $company->address }}</div>
-            <div class="flex justify-between items-center">
-              <div class="text-xxs text-cyan-500">{{ $company->website }}</div>
-              <svg @click="showEmployees{{ $company->id }} = ! showEmployees{{ $company->id }}" class="h-4 w-4 text-cyan-700" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 13l-3 3m0 0l-3-3m3 3V8m0 13a9 9 0 110-18 9 9 0 010 18z" />
-              </svg>
-            </div>
+            <div class="text-xxs text-gray-500">{{ $company->address }}</div>
+            <div class="text-xxs text-cyan-500">{{ $company->website }}</div>
           </td>
           <td class="py-1 px-2">{{ $company->email }}</td>
           <td class="py-1 px-2">{{ $company->country }}</td>
@@ -95,30 +89,6 @@
             <button @click="showConfirmModal{{ $company->id }} = true">Delete</button>
           </td>
         </tr>
-
-        <tr class="bg-gray-50 border-b" x-show="showEmployees{{$company->id}}" x-cloak>
-          <td colspan="8">
-            <table class="table-auto text-sm my-5">
-              <thead class="">
-                <tr class="uppercase text-left text-sm">
-                  <th class="pl-6 pt-2">Name</th>
-                  <th class="pt-2 px-2">Email</th>
-                </tr>
-              </thead>
-              <tbody class="py-3">
-                @foreach ($company->employees as $employee)
-                <tr class="text-xs">
-                  <td class="py-1 pl-6 whitespace-nowrap ">
-                    <a href="{{ route('employee.show', $employee) }}" class="font-semibold">{{ $employee->name }}</a>
-                  </td>
-                  <td class="text-cyan-500 py-1 px-2 " style="vertical-align: middle">{{ $employee->email }}</td>
-                </tr>
-                @endforeach
-              </tbody>
-            </table>
-          </td>
-        </tr>
-
         @endforeach
       </tbody>
     </table>
@@ -134,6 +104,37 @@
       />
     </div>
     @endforeach
+
+    <div >
+    @foreach($companies as $company)
+      <table  class="table-fixed text-sm my-4" x-show="showEmployeesTab === {{ $company->id }}">
+        <thead>
+          <tr class="uppercase text-left flex">
+            <th class="p-2">Name</th>
+            <th class="p-2">Company</th>
+            <th class="p-2">Country</th>
+            <th class="p-2">Notes</th>
+          </tr>
+        </thead>
+        <tbody class="py-3 divide-y">
+        @foreach ($company->employees as $employee)
+          <tr class="text-xs">
+            <td class="py-1 px-2">
+              <a href="{{ route('employee.show', $employee) }}">
+                <div class="text-sm font-semibold mb-1">{{ $employee->name }}</div>
+                <div class="text-cyan-500">{{ $employee->email }}</div>
+              </a>
+            </td>
+            <td class="font-semibold py-1 px-2">{{ $employee->company->name }}</td>
+            <td class="py-1 px-2" style="vertical-align: middle">{{ $employee->company->country }}</td>
+            <td class="py-1 px-2">{{ $employee->notes }}</td>
+          </tr>
+        @endforeach
+        </tbody>
+      </table>
+    @endforeach
   </div>
+</div>
+
 
 </x-layout>
